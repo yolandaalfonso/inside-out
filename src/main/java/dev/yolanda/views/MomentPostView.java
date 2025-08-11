689506167180a334687460cf0bee66027c50e208
Yolanda <yolandaalfonsolopez@gmail.com>
2025-08-11 14:45:15 +0200

    feat: add menu to enter a moment

--- src/main/java/dev/yolanda/views/MomentPostView.java
+++ src/main/java/dev/yolanda/views/MomentPostView.java
@@ -1,5 +1,37 @@
 package dev.yolanda.views;
 
-public class MomentPostView {
-    
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import dev.yolanda.controllers.ModelController;
+import dev.yolanda.dtos.MomentDTO;
+
+public class MomentPostView extends View{
+
+    private static ModelControllerController CONTROLLER = ModelControllerSingleton.getInstance();
+
+    public static void printStoreMenu() {
+        System.out.println("Ingrese el título:");
+        String momentTitle = SCANNER.next();
+
+        System.out.println("Ingresa la fecha(dd/mm/year):");
+        String date = SCANNER.next();
+        SimpleDateFormat inputFormat = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            Date inputDate = inputFormat.parse(date);
+            System.out.println("String to Date: " + inputDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+
+        System.out.println("Ingrese la descripción:");
+        String description = SCANNER.next();
+
+        MomentDTO moment = new MomentDTO(momentTitle, date, description);
+        CONTROLLER.StoreMoment(moment);
+    }
+
 }
